a01893b0e5e8983bef5e5e2958788583a3784b58
Gaurav Naresh Sunthwal <70788155+codewithgaurav252005@users.noreply.github.com>
2024-03-21 02:29:52 +0530

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withSitemap = require("next-sitemap");
 
-module.exports = withSitemap({
-  // Your Next.js config options
-  // Add sitemap configuration here
-  sitemap: {
-    hostname: "https://generate-readme.vercel.app/",
-  },
-});
 const nextConfig = {};
 
 export default nextConfig;
